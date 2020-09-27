f1ca8c3f0728459591c918d8cdd38f3d2b5eac90
Whenix <396651902@qq.com>
2020-09-27 23:53:07 +0800

    add more stuff

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Youngs注
+/// 此脚本引用自 https://github.com/danielzeller/Lowpoly-Water-Unity
+/// 暂时未做任何修改，目前搭载上去效果不好，需要做几点：
+/// 1、专门写一个shader给这个脚本
+/// 2、重新实现这个脚本的噪声函数
+/// 3、边缘泡沫的问题(此逻辑需要重新好好看一下原项目的shader是怎么写的)
+/// 
+/// 所以暂时保留但不用
+/// </summary>
+public class Water : MonoBehaviour
+{
+
+	Vector3 waveSource1 = new Vector3 (2.0f, 0.0f, 2.0f);
+	public float waveFrequency = 0.53f;
+	public float waveHeight = 0.48f;
+	public float waveLength = 0.71f;
+	public bool edgeBlend=true;
+	public bool forceFlatShading =true;
+	Mesh mesh;
+	Vector3[] verts;
+ 
+	void Start ()
+	{
+		Camera.main.depthTextureMode |= DepthTextureMode.Depth;
+		MeshFilter mf = GetComponent<MeshFilter> ();  
+		makeMeshLowPoly (mf);
+ 
+	}
+
+	MeshFilter makeMeshLowPoly (MeshFilter mf)
+	{
+		mesh = mf.sharedMesh;//Change to sharedmesh? 
+		Vector3[] oldVerts = mesh.vertices;
+		int[] triangles = mesh.triangles;
+		Vector3[] vertices = new Vector3[triangles.Length];
+		for (int i = 0; i < triangles.Length; i++) {
+			vertices [i] = oldVerts [triangles [i]];
+			triangles [i] = i;
+		}
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.RecalculateBounds ();
+		mesh.RecalculateNormals ();
+		verts = mesh.vertices;
+		return mf;
+	}
+
+	void setEdgeBlend ()
+	{
+		if (!SystemInfo.SupportsRenderTextureFormat (RenderTextureFormat.Depth)) {
+			edgeBlend = false;
+		}
+		if (edgeBlend) {
+			Shader.EnableKeyword ("WATER_EDGEBLEND_ON"); 
+			if (Camera.main) {
+				Camera.main.depthTextureMode |= DepthTextureMode.Depth;
+			}
+		}
+		else { 
+			Shader.DisableKeyword ("WATER_EDGEBLEND_ON");
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{ 
+		CalcWave ();
+		setEdgeBlend (); 
+	}
+
+	void CalcWave ()
+	{
+		for (int i = 0; i < verts.Length; i++) {
+			Vector3 v = verts [i];
+			v.y = 0.0f;
+			float dist = Vector3.Distance (v, waveSource1);
+			dist = (dist % waveLength) / waveLength;
+			v.y = waveHeight * Mathf.Sin (Time.time * Mathf.PI * 2.0f * waveFrequency
+			+ (Mathf.PI * 2.0f * dist));
+			verts [i] = v;
+		}
+		mesh.vertices = verts;
+		mesh.RecalculateNormals (); 
+		mesh.MarkDynamic ();
+	
+		GetComponent<MeshFilter> ().mesh = mesh;
+	}
+}
